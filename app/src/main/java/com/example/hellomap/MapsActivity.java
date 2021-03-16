c74b64ae3b5ee20000f9c1250220b6e9efd5c414
Xavier <xaavier@protonmail.com>
2021-03-16 15:42:51 +0100

    Change location

--- app/src/main/java/com/example/hellomap/MapsActivity.java
+++ app/src/main/java/com/example/hellomap/MapsActivity.java
@@ -38,9 +38,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng eps = new LatLng(41.608366040583306, 0.6234402336874381);
+        mMap.addMarker(new MarkerOptions().position(eps).title("Marker in EPS, Lleida"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(eps));
     }
 }
