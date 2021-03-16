9cdf7731a0b4617c33a0bcf705d85b892d6f1dc9
Xavier <xaavier@protonmail.com>
2021-03-16 21:20:41 +0100

    Added Layers functionality where we can switch from Hybrid/Satellite/Terrain/Normal views of the map

--- app/src/main/java/com/example/hellomap/MapsActivity.java
+++ app/src/main/java/com/example/hellomap/MapsActivity.java
@@ -6,9 +6,16 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -18,19 +25,47 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import static com.google.android.gms.maps.GoogleMap.MAP_TYPE_HYBRID;
+import static com.google.android.gms.maps.GoogleMap.MAP_TYPE_NONE;
+import static com.google.android.gms.maps.GoogleMap.MAP_TYPE_NORMAL;
+import static com.google.android.gms.maps.GoogleMap.MAP_TYPE_SATELLITE;
+import static com.google.android.gms.maps.GoogleMap.MAP_TYPE_TERRAIN;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
         GoogleMap.OnMyLocationButtonClickListener,
         GoogleMap.OnMyLocationClickListener,
-        ActivityCompat.OnRequestPermissionsResultCallback{
+        ActivityCompat.OnRequestPermissionsResultCallback,
+        AdapterView.OnItemSelectedListener {
 
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private boolean permissionDenied = false;
+
+    private CheckBox mTrafficCheckbox;
+    private CheckBox mMyLocationCheckbox;
+    private CheckBox mBuildingsCheckbox;
+    private CheckBox mIndoorCheckbox;
+    private Spinner mSpinner;
+    private boolean mShowPermissionDeniedDialog = false;
+
     private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        mSpinner = findViewById(R.id.layers_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.layers_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(adapter);
+        mSpinner.setOnItemSelectedListener(this);
+
+        mTrafficCheckbox = findViewById(R.id.traffic);
+        mMyLocationCheckbox = findViewById(R.id.my_location);
+        mBuildingsCheckbox = findViewById(R.id.buildings);
+        mIndoorCheckbox = findViewById(R.id.indoor);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -53,6 +88,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng eps = new LatLng(41.608366040583306, 0.6234402336874381);
         map.addMarker(new MarkerOptions().position(eps).title("Marker in EPS, Lleida"));
         map.moveCamera(CameraUpdateFactory.newLatLng(eps));
+
+        updateMapType();
+        updateTraffic();
+        updateMyLocation();
+        updateBuildings();
+        updateIndoor();
 
         map.setOnMyLocationButtonClickListener(this);
         map.setOnMyLocationClickListener(this);
@@ -91,6 +132,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     // [START maps_check_location_permission_result]
+    @SuppressLint("MissingPermission")
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
@@ -100,11 +142,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (PermissionUtils.isPermissionGranted(permissions, grantResults, Manifest.permission.ACCESS_FINE_LOCATION)) {
             // Enable the my location layer if the permission has been granted.
             enableMyLocation();
+            map.setMyLocationEnabled(true);
+            mMyLocationCheckbox.setChecked(true);
         } else {
             // Permission was denied. Display an error message
             // [START_EXCLUDE]
             // Display the missing permission error dialog when the fragments resume.
             permissionDenied = true;
+            mShowPermissionDeniedDialog = true;
             // [END_EXCLUDE]
         }
     }
@@ -118,6 +163,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             showMissingPermissionError();
             permissionDenied = false;
         }
+        if (mShowPermissionDeniedDialog) {
+            PermissionUtils.PermissionDeniedDialog
+                    .newInstance(false).show(getSupportFragmentManager(), "dialog");
+            mShowPermissionDeniedDialog = false;
+        }
     }
 
     /**
@@ -126,5 +176,120 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private void showMissingPermissionError() {
         PermissionUtils.PermissionDeniedDialog
                 .newInstance(true).show(getSupportFragmentManager(), "dialog");
+    }
+
+    private boolean checkReady() {
+        if (map == null) {
+            Toast.makeText(this, R.string.map_not_ready, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Called when the Traffic checkbox is clicked.
+     */
+    public void onTrafficToggled(View view) {
+        updateTraffic();
+    }
+
+    private void updateTraffic() {
+        if (!checkReady()) {
+            return;
+        }
+        map.setTrafficEnabled(mTrafficCheckbox.isChecked());
+    }
+
+    /**
+     * Called when the MyLocation checkbox is clicked.
+     */
+    public void onMyLocationToggled(View view) {
+        updateMyLocation();
+    }
+
+    @SuppressLint("MissingPermission")
+    private void updateMyLocation() {
+        if (!checkReady()) {
+            return;
+        }
+
+        if (!mMyLocationCheckbox.isChecked()) {
+            map.setMyLocationEnabled(false);
+            return;
+        }
+
+        // Enable the location layer. Request the location permission if needed.
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            map.setMyLocationEnabled(true);
+        } else {
+            // Uncheck the box until the layer has been enabled and request missing permission.
+            mMyLocationCheckbox.setChecked(false);
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, false);
+        }
+    }
+
+    /**
+     * Called when the Buildings checkbox is clicked.
+     */
+    public void onBuildingsToggled(View view) {
+        updateBuildings();
+    }
+
+    private void updateBuildings() {
+        if (!checkReady()) {
+            return;
+        }
+        map.setBuildingsEnabled(mBuildingsCheckbox.isChecked());
+    }
+
+    /**
+     * Called when the Indoor checkbox is clicked.
+     */
+    public void onIndoorToggled(View view) {
+        updateIndoor();
+    }
+
+    private void updateIndoor() {
+        if (!checkReady()) {
+            return;
+        }
+        map.setIndoorEnabled(mIndoorCheckbox.isChecked());
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        updateMapType();
+    }
+
+    private void updateMapType() {
+        // No toast because this can also be called by the Android framework in onResume() at which
+        // point map may not be ready yet.
+        if (map == null) {
+            return;
+        }
+
+        String layerName = ((String) mSpinner.getSelectedItem());
+        if (layerName.equals(getString(R.string.normal))) {
+            map.setMapType(MAP_TYPE_NORMAL);
+        } else if (layerName.equals(getString(R.string.hybrid))) {
+            map.setMapType(MAP_TYPE_HYBRID);
+
+
+        } else if (layerName.equals(getString(R.string.satellite))) {
+            map.setMapType(MAP_TYPE_SATELLITE);
+        } else if (layerName.equals(getString(R.string.terrain))) {
+            map.setMapType(MAP_TYPE_TERRAIN);
+        } else if (layerName.equals(getString(R.string.none_map))) {
+            map.setMapType(MAP_TYPE_NONE);
+        } else {
+            Log.i("LDA", "Error setting layer with name " + layerName);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Do nothing.
     }
 }
